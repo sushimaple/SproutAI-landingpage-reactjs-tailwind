3cc30b7afdbf5e4f20007d859f643970df693fcf
aaron <aaronsamonte02@gmail.com>
2025-12-12 10:50:23 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-950 text-white overflow-hidden">
+      <Navbar />
+      <Hero />
+      <Features />
+      <Pricing />
+      <Testimonials />
+      <Footer />
+    </div>
+  )
+}
+
+export default App
